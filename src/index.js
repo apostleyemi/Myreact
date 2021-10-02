0ce35f7d9ffa5d6115975691c481f7889bec0a59
Adedayo <080561.cse@gmail.com>
2021-10-02 16:34:35 +0100

    function components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+/*  class tutorial */
+class Employee extends React.Component{
 
+  render()
+  {
+      return <div>
+        <h1>Employee Infomation</h1>
+        <p>Employeen ID : {this.props.Id}</p>
+        <p>Employeen Name : {this.props.Name}</p>
+        <p>Employeen Gender : {this.props.Gender}</p>
+        <p>Employeen Phone : {this.props.Phone}</p>
+
+<Department deptName={this.props.deptName} deptHead={this.props.deptHead}></Department>
+      </div>
+  }
+}
+
+
+class Department extends React.Component{
+render(){
+
+  return <div>
+    <h2> Department Details</h2>
+    <p>
+      <label> Department Name is : {this.props.deptName}</label>
+    </p>
+    <p>
+      <label> Department Head is : {this.props.deptHead}</label>
+    </p>
+  </div>
+
+}
+
+
+}
+
+  
+  const element=<Employee Id="0002" Name="Akinola Abayomi" Gender="Male" Phone="08067013148" deptName="Software Unit" deptHead="ICT"></Employee>
+
+
+ReactDOM.render(element,document.getElementById("root"));
+
+
+/* 
 var  DisplayEmployeeInfo=(employee)=>
 { return <div>
   <h1>Employee Information</h1>
@@ -38,3 +81,4 @@ const element=<DisplayEmployeeInfo Id="0001" Name="Joy Apata" gender="Male" Phon
  headName="ICT"></DisplayEmployeeInfo>
 
 ReactDOM.render(element,document.getElementById("root"));
+ */
